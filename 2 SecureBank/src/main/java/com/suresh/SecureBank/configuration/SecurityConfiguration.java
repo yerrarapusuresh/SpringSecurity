7bbba182a4a3c113e1b4f34cdac2e502633afee6
Yerrarapu Suresh <yerrarapusuresh@gmail.com>
2022-12-19 22:41:05 +0530

    created users by injecting InMemoryUserDetailsManager

--- 2 SecureBank/src/main/java/com/suresh/SecureBank/configuration/SecurityConfiguration.java
+++ 2 SecureBank/src/main/java/com/suresh/SecureBank/configuration/SecurityConfiguration.java
@@ -3,6 +3,9 @@ package com.suresh.SecureBank.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -11,6 +14,13 @@ public class SecurityConfiguration {
 	@Bean
 	SecurityFilterChain securityConfig(HttpSecurity http) throws Exception {
 		return http.authorizeHttpRequests().requestMatchers("/myBalance","/myAccount").authenticated().requestMatchers("/notices", "/contacts").permitAll().and().httpBasic().and().formLogin().and().build();
+	}
+	
+	@Bean
+	InMemoryUserDetailsManager createInMemoryUsers() {
+	 UserDetails adminUser = User.withDefaultPasswordEncoder().username("suresh").password("****").authorities("admin").build();
+	 UserDetails user = User.withDefaultPasswordEncoder().username("user").password("****").authorities("user").build();
+	 return new InMemoryUserDetailsManager(adminUser, user);
 	}
 
 }
