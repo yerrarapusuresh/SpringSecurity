1c4a5701bab910b64a79c67b97a11c71ee8a5059
Yerrarapu Suresh <yerrarapusuresh@gmail.com>
2022-12-22 22:38:57 +0530

    enbled security by using default users table

--- 2 SecureBank/src/main/java/com/suresh/SecureBank/SecureBankApplication.java
+++ 2 SecureBank/src/main/java/com/suresh/SecureBank/SecureBankApplication.java
@@ -2,6 +2,7 @@ package com.suresh.SecureBank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
 public class SecureBankApplication {
--- 2 SecureBank/src/main/java/com/suresh/SecureBank/configuration/SecurityConfiguration.java
+++ 2 SecureBank/src/main/java/com/suresh/SecureBank/configuration/SecurityConfiguration.java
@@ -1,11 +1,14 @@
 package com.suresh.SecureBank.configuration;
 
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -16,11 +19,22 @@ public class SecurityConfiguration {
 		return http.authorizeHttpRequests().requestMatchers("/myBalance","/myAccount").authenticated().requestMatchers("/notices", "/contacts").permitAll().and().httpBasic().and().formLogin().and().build();
 	}
 	
+//	@Bean
+//	InMemoryUserDetailsManager createInMemoryUsers() {
+//	 UserDetails adminUser = User.withDefaultPasswordEncoder().username("suresh").password("****").authorities("admin").build();
+//	 UserDetails user = User.withDefaultPasswordEncoder().username("user").password("****").authorities("user").build();
+//	 return new InMemoryUserDetailsManager(adminUser, user);
+//	}
+	
 	@Bean
-	InMemoryUserDetailsManager createInMemoryUsers() {
-	 UserDetails adminUser = User.withDefaultPasswordEncoder().username("suresh").password("****").authorities("admin").build();
-	 UserDetails user = User.withDefaultPasswordEncoder().username("user").password("****").authorities("user").build();
-	 return new InMemoryUserDetailsManager(adminUser, user);
+	UserDetailsService jdbcUsers(DataSource dataSource) {
+		return new JdbcUserDetailsManager(dataSource);
+	}
+	
+	
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return NoOpPasswordEncoder.getInstance();
 	}
 
 }
